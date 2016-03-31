7df85c946816c5ed6e2d6359e2450b6224ecb815
kankanla <koyi_s@hotmail.com>
2016-04-01 00:02:12 +0900

    PHP $_GET (javascrint)

--- php_get_function.js
+++ php_get_function.js
@@ -1,17 +1,18 @@
 
 
 function $_GET(q){
-	var array = {};
+	var temp = {};
 	var search = location.search.split('?')[1].split('&');
 	for(var i = 0; i < search.length; i++){
-		var x = search[i].split('=');
-		array[x[0]]=x[1];
+		var para = search[i].split('=');
+		temp[para[0]]=para[1];
 	}
 
 	if(q){
-		return array[q];
+		return temp[q];
+		// return temp.q;	// NG
 	}else{
-		return array;
+		return temp;
 	}
 }
 
