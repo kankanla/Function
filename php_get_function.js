31f902a5f4ff0bd98fa8da3e7dc0def586e1bd45
kankanla <koyi_s@hotmail.com>
2016-04-01 20:11:08 +0900

    Update php_get_function.js

--- php_get_function.js
+++ php_get_function.js
@@ -1,19 +1,19 @@
 function $_GET(q){
-    if(location.search.length > 0){
-    	var temp = {};
-        var search = location.search.split('?')[1].split('&');
-     	    for(var i = 0; i < search.length; i++){
-                var para = search[i].split('=');
-                temp[para[0]]=para[1];
-        	}
+	if(location.search.length > 0){
+		var temp = {};
+		var search = location.search.split('?')[1].split('&');
+			for(var i = 0; i < search.length; i++){
+				var para = search[i].split('=');
+				temp[para[0]]=para[1];
+			}
 
-        if(q && temp[q]){
-        	// youfunction(temp[q]);
-                // return temp.q; // NG
-                return temp[q];
-        }else{
-        	// console.log(temp);
-                return temp;
-        }
-    }
+		if(temp[q]){
+			// youfunction(temp[q]);
+			// return temp.q; // NG
+			return temp[q];
+		}else{
+			// console.log(temp);
+			return temp;
+		}
+	}
 }
